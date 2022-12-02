ae0d8365ae040966d4b98d62a51b4423ff5039c6
Amanbek-uulu-Kytman <gidhubkytmanbek@mail.ru>
2022-12-02 17:40:44 +0600

    first commit

---
+++ high_school/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=30,verbose_name='имя')
+    class_name = models.CharField(max_length=30, choices=(
+        ('a', 'a_class'),
+        ('b', 'b_class'),
+        ('c', 'c_class')
+
+    ), verbose_name='Название класса')
+
+    def __str__(self):
+        return self.name
+
+
+class Pupil(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    birth_date = models.DateField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
